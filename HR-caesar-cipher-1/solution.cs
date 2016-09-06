d9709b2551ef4203e66bf924a563fea0773ae978
Doug Swisher <doug.swisher@dowjones.com>
2016-09-06 08:10:27 -0500

    Caesar-cipher solution

--- HR-caesar-cipher-1/solution.cs
+++ HR-caesar-cipher-1/solution.cs
@@ -1,16 +1,37 @@
 
 using System;
+using System.Text;
 
 public class Solution
 {
+	private static char[] _map;
+
+	static Solution()
+	{
+		_map = new char[26];
+		for (var i = 0; i < _map.Length; i++) { _map[i] = (char)(i + 'a'); }
+	}
+
+
 	public static void Main(string[] args)
 	{
-		var n = int.Parse(Console.ReadLine());
+		Console.ReadLine(); // skip unused N
 		var s = Console.ReadLine();
 		var k = int.Parse(Console.ReadLine());
 
-		// TODO
-		Console.WriteLine(s);
+		StringBuilder result = new StringBuilder();
+		foreach (var c in s) { result.Append(Encode(c, k)); }
+
+		Console.WriteLine(result);
+	}
+
+
+	private static char Encode(char c, int k)
+	{
+		if (!Char.IsLetter(c)) { return c; }
+		if (Char.IsUpper(c)) { return Char.ToUpper(Encode(Char.ToLower(c), k)); }
+
+		return _map[((int)c - (int)'a' + k) % _map.Length];
 	}
 }
 
