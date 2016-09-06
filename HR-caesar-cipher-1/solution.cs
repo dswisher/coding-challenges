8a459f5f2318d2740d3c71a71bf256bbff5f3d2e
Doug Swisher <big.swish@gmail.com>
2016-09-05 22:13:05 -0500

    Add new problem

---
+++ HR-caesar-cipher-1/solution.cs
@@ -0,0 +1,16 @@
+
+using System;
+
+public class Solution
+{
+	public static void Main(string[] args)
+	{
+		var n = int.Parse(Console.ReadLine());
+		var s = Console.ReadLine();
+		var k = int.Parse(Console.ReadLine());
+
+		// TODO
+		Console.WriteLine(s);
+	}
+}
+
