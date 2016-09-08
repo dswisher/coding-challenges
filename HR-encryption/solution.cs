5b58f60dffafaf49d8911a9993d3071044d0de96
Doug Swisher <doug.swisher@dowjones.com>
2016-09-08 07:58:18 -0500

    A little code cleanup

--- HR-encryption/solution.cs
+++ HR-encryption/solution.cs
@@ -7,18 +7,16 @@ public class Solution
 	{
 		string s = Console.ReadLine().Replace(" ", string.Empty);
 
-		// Determine the number of rows and columns (L = s.Length):
+		// Determine the number of rows and columns. We don't actually need to know the rows, though...
 		var sqrt = Math.Sqrt(s.Length);
 		var lb = (int)Math.Floor(sqrt); var ub = (int)Math.Ceiling(sqrt);
-		var rows = lb;
 		var cols = lb;
 		if (lb * lb < s.Length)
 		{
-			if ((lb * ub) >= s.Length) { cols = ub; }
-			else { rows = ub; cols = ub; }
+			cols = ub;
 		}
 
-		// Print the encryption
+		// Print encrypted text...
 		for (var i = 0; i < cols; i++)
 		{
 			if (i > 0) { Console.Write(' '); }
