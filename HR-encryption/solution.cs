cde676d2db04176148681aac922ceedd7d889715
Doug Swisher <doug.swisher@dowjones.com>
2016-09-08 07:54:25 -0500

    Encryption passing initial tests

--- HR-encryption/solution.cs
+++ HR-encryption/solution.cs
@@ -8,25 +8,21 @@ public class Solution
 		string s = Console.ReadLine().Replace(" ", string.Empty);
 
 		// Determine the number of rows and columns (L = s.Length):
-		//  * floor(sqrt(L)) <= rows <= cols <= ceil(sqrt(L))
-		//  * ensure that rows * cols >= L
-		//  * if multiple grids satisfy the above, choose one with smallest area (rows * cols)
-		// TODO
-		var rows = 7;
-		var cols = 8;
+		var sqrt = Math.Sqrt(s.Length);
+		var lb = (int)Math.Floor(sqrt); var ub = (int)Math.Ceiling(sqrt);
+		var rows = lb;
+		var cols = lb;
+		if (lb * lb < s.Length)
+		{
+			if ((lb * ub) >= s.Length) { cols = ub; }
+			else { rows = ub; cols = ub; }
+		}
 
 		// Print the encryption
 		for (var i = 0; i < cols; i++)
 		{
-			if (i > 0)
-			{
-				Console.Write(' ');
-			}
-
-			for (var j = i; j < s.Length; j += cols)
-			{
-				Console.Write(s[j]);
-			}
+			if (i > 0) { Console.Write(' '); }
+			for (var j = i; j < s.Length; j += cols) { Console.Write(s[j]); }
 		}
 
 		Console.WriteLine();
