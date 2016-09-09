7a54542821455915cba5ed4c0af42bb73439a05d
Doug Swisher <doug.swisher@dowjones.com>
2016-09-09 10:42:38 -0500

    Finish circular-array-rotation

--- HR-circular-array-rotation/solution.cs
+++ HR-circular-array-rotation/solution.cs
@@ -1,11 +1,26 @@
 
 using System;
+using System.Linq;
 
 public class Solution
 {
 	public static void Main(string[] args)
 	{
-		// TODO
+		var bits = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToArray();
+		var n = bits[0];
+		var k = bits[1];
+		var q = bits[2];
+		var a = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToArray();
+
+		for (var i = 0; i < q; i++)
+		{
+			var m = int.Parse(Console.ReadLine());
+
+			var idx = m - k;
+			while (idx < 0) { idx += n; }
+
+			Console.WriteLine(a[idx]);
+		}
 	}
 }
 
