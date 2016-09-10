3b2db4bce0f765fec78852ceaf9d1be7c7a4d04c
Doug Swisher <big.swish@gmail.com>
2016-09-09 22:42:46 -0500

    Finish flatland-space-stations

--- HR-flatland-space-stations/solution.cs
+++ HR-flatland-space-stations/solution.cs
@@ -1,11 +1,29 @@
 
 using System;
+using System.Linq;
 
 public class Solution
 {
 	public static void Main(string[] args)
 	{
-		// TODO
+		var bits = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToArray();
+		var n = bits[0];
+		// var m = bits[1];
+
+		var c = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).OrderBy(x => x).ToArray();
+
+		// Edges
+		var max = Math.Max(c[0], n - c[c.Length - 1] - 1);
+
+		// Middles
+		for (var i = 0; i < c.Length - 1; i++)
+		{
+			var dist = (c[i + 1] - c[i]) / 2;
+
+			max = Math.Max(max, dist);
+		}
+
+		Console.WriteLine(max);
 	}
 }
 
