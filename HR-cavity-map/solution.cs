a32c1fe141108036fecaca8e3af514250058b70b
Doug Swisher <doug.swisher@dowjones.com>
2016-09-12 08:35:38 -0500

    Finish cavity-map

--- HR-cavity-map/solution.cs
+++ HR-cavity-map/solution.cs
@@ -17,56 +17,23 @@ public class Solution
 			}
 		}
 
-		// Cavity search!
-		for (var i = 1; i < n - 1; i++)
-		{
-			for (var j = 1; j < n - 1; j++)
-			{
-				if (IsCavity(map, i, j))
-				{
-					map[i,j] = 0;
-				}
-			}
-		}
-
-		// Print the map
 		for (var i = 0; i < n; i++)
 		{
 			for (var j = 0; j < n; j++)
 			{
-				if (map[i,j] == 0)
+				var d = map[i,j];
+				if (i == 0 || j == 0 || i == n - 1 || j == n - 1
+					|| d <= map[i - 1, j] || d <= map[i + 1, j] || d <= map[i, j - 1] || d <= map[i, j + 1])
 				{
-					Console.Write('X');
+					Console.Write(d);
 				}
 				else
 				{
-					Console.Write(map[i,j]);
+					Console.Write('X');
 				}
 			}
 			Console.WriteLine();
 		}
-	}
-
-
-	private static bool IsCavity(int[,] map, int i, int j)
-	{
-		for (var di = -1; di <= 1; di++)
-		{
-			for (var dj = -1; dj <= 1; dj++)
-			{
-				if (di == 0 && dj == 0)
-				{
-					continue;
-				}
-
-				if (map[i + di, j + dj] > 0 && map[i + di, j + dj] > map[i,j])
-				{
-					return false;
-				}
-			}
-		}
-
-		return true;
 	}
 }
 
