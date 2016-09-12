97051c671615157b33e91c928ff525094d4035cb
Doug Swisher <doug.swisher@dowjones.com>
2016-09-12 07:36:00 -0500

    Work on cavity-map

--- HR-cavity-map/solution.cs
+++ HR-cavity-map/solution.cs
@@ -5,8 +5,68 @@ public class Solution
 {
 	public static void Main(string[] args)
 	{
+		// Load the map
 		var n = int.Parse(Console.ReadLine());
-		// TODO
+		var map = new int[n,n];
+		for (var i = 0; i < n; i++)
+		{
+			var line = Console.ReadLine();
+			for (var j = 0; j < n; j++)
+			{
+				map[i,j] = int.Parse(line[j].ToString());
+			}
+		}
+
+		// Cavity search!
+		for (var i = 1; i < n - 1; i++)
+		{
+			for (var j = 1; j < n - 1; j++)
+			{
+				if (IsCavity(map, i, j))
+				{
+					map[i,j] = 0;
+				}
+			}
+		}
+
+		// Print the map
+		for (var i = 0; i < n; i++)
+		{
+			for (var j = 0; j < n; j++)
+			{
+				if (map[i,j] == 0)
+				{
+					Console.Write('X');
+				}
+				else
+				{
+					Console.Write(map[i,j]);
+				}
+			}
+			Console.WriteLine();
+		}
+	}
+
+
+	private static bool IsCavity(int[,] map, int i, int j)
+	{
+		for (var di = -1; di <= 1; di++)
+		{
+			for (var dj = -1; dj <= 1; dj++)
+			{
+				if (di == 0 && dj == 0)
+				{
+					continue;
+				}
+
+				if (map[i + di, j + dj] > 0 && map[i + di, j + dj] > map[i,j])
+				{
+					return false;
+				}
+			}
+		}
+
+		return true;
 	}
 }
 
