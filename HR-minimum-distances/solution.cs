a59a1d49e23bf1177780d8512ec1d3a375a0a802
Doug Swisher <big.swish@gmail.com>
2016-09-17 21:06:39 -0500

    Finish minimum-distances

--- HR-minimum-distances/solution.cs
+++ HR-minimum-distances/solution.cs
@@ -1,11 +1,27 @@
 
 using System;
+using System.Linq;
 
 public class Solution
 {
 	public static void Main(string[] args)
 	{
-		// TODO
+		var n = int.Parse(Console.ReadLine());
+		var a = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToArray();
+
+		var min = int.MaxValue;
+		for (var i = 0; i < n - 1; i++)
+		{
+			for (var j = i + 1; j < n; j++)
+			{
+				if ((a[i] == a[j]) && (j - i < min))
+				{
+					min = j - i;
+				}
+			}
+		}
+
+		Console.WriteLine(min == int.MaxValue ? -1 : min);
 	}
 }
 
