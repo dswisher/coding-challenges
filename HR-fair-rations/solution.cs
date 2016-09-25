0cb9ce89962d6247bc3d1e1886e9906b54ffbe5a
Doug Swisher <big.swish@gmail.com>
2016-09-24 22:16:32 -0500

    Finish fair-rations

--- HR-fair-rations/solution.cs
+++ HR-fair-rations/solution.cs
@@ -1,11 +1,32 @@
 
 using System;
+using System.Linq;
 
 public class Solution
 {
 	public static void Main(string[] args)
 	{
-		// TODO
+		var N = int.Parse(Console.ReadLine());
+		var b = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToArray();
+
+		if (b.Sum() % 2 == 1)
+		{
+			Console.WriteLine("NO");
+		}
+		else
+		{
+			var count = 0;
+			for (var i = 0; i < N - 1; i++)
+			{
+				if (b[i] % 2 == 1)
+				{
+					b[i] += 1;
+					b[i+1] += 1;
+					count += 2;
+				}
+			}
+			Console.WriteLine(count);
+		}
 	}
 }
 
